ce2a2d0f1216b2584c3d822534bcf565958a2d60
boms606 <sarialm@hotmail.com>
2020-03-13 17:33:37 +0100

    filling

---
+++ first_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+usernam=$(whoami)
+
+
+#create directories and set path to accept own scripts globally
+mkdir ~/scripts
+mkdir ~/scripts/bash
+export PATH=$PATH:/home/$usernam/scripts/bash/ 
+sudo echo "" >> /etc/profile
+sudo echo "export PATH=$PATH:/home/$usernam/scripts/bash/" >> /etc/profile
+
+#copy scripts
+cp scrip/* ~/scripts/bash/
+
+#install some stuff
+sudo apt update
+sudo apt upgrade
+sudo apt update
+sudo apt install tmux htop snapd
+sudo apt update
+
+#set personalisation variables
+sudo echo "set -g default-terminal \"screen-256color\"" > /etc/tmux.conf
--- testfile.sh
+++ testfile.sh
@@ -2,3 +2,5 @@
 
 echo "This is a Test file, which prints out the arguments. Your argument was: $1"
 #Just one more comment!
+
+#more

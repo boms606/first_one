b549585aeea900795bd3d9d13ef1347d84c40529
boms606 <sarialm@hotmail.com>
2020-03-13 17:47:02 +0100

    ha

--- prequisites.sh
+++ prequisites.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt install git
 git config --global user.name "boms606" 
 mkdir scripts
@@ -5,6 +7,6 @@ cd scripts
 mkdir bomsgit
 git clone git://github.com/boms606/first_one.git
 cd first_one
-sudo mkdir +x first_setup.sh
+sudo chmod +x first_setup.sh
 ./first_setup.sh
 

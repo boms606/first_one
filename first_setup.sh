a3aea93e0019748d1d75904902bb337dcab082e2
boms606 <sarialm@hotmail.com>
2020-03-13 20:30:16 +0100

    al

--- first_setup.sh
+++ first_setup.sh
@@ -7,20 +7,20 @@ usernam=$(whoami)
 mkdir ~/scripts
 mkdir ~/scripts/bash
 export PATH=$PATH:/home/$usernam/scripts/bash/ 
-sudo echo "" >> /etc/profile
-sudo echo "export PATH=$PATH:/home/$usernam/scripts/bash/" >> /etc/profile
+echo "" >> /etc/profile
+echo "export PATH=$PATH:/home/$usernam/scripts/bash/" >> /etc/profile
 
 #copy scripts
 cp scrip/* ~/scripts/bash/
 
 #install some stuff
-sudo apt update
-sudo apt upgrade
-sudo apt update
-sudo apt install tmux htop snapd
-sudo apt update
+apt update
+apt upgrade
+apt update
+apt install tmux htop snapd
+apt update
 
 #set personalisation variables
-sudo echo "set -g default-terminal \"screen-256color\"" > /etc/tmux.conf
+echo "set -g default-terminal \"screen-256color\"" > /etc/tmux.conf
 
-to be continued later
+#to be continued later

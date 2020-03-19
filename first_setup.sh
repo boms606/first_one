1cc6624a976ca5c6b55726574c0876a4a55ded1a
boms606 <mumu@Desktop-EVO860.localdomain>
2020-03-19 13:40:47 +0100

    alamk changes

--- first_setup.sh
+++ first_setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# todo: - find a way to check whether working directory is first_one/
+#       - add timestamps to error.log logs
+#       - delete errors.log when no error or prevent it from being created
+
 #redirect errors to logfile named "errors"
-exec 2> errors
+exec 2> errors.log
 
 #usernam=$(whoami)
 usernam=$1
@@ -27,5 +31,9 @@ export PATH=$PATH:/home/$usernam/scripts/bash/
 
 #set personalisation variables
 echo "set -g default-terminal \"screen-256color\"" > /etc/tmux.conf
+
+#handover to new user
+chown -R $usernam:$usernam ../first_one/
+chown -R $usernam:$usernam /home/$usernam/scripts/
 
 #to be continued later

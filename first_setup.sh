3080ac63a069c2bcc1c00bd54c0e7000220e683b
boms606 <sarialm@hotmail.com>
2020-03-23 14:30:22 +0100

    fixl53

--- first_setup.sh
+++ first_setup.sh
@@ -45,12 +45,12 @@ echo "set -g default-terminal \"screen-256color\"" > /etc/tmux.conf
 echo -e "a 00006f\ng logo 6f0000\ng multimedia 006f00\ng indicators 006f00\nc" > /etc/g810-led/profile
 
 #load settings from backup if necessary
-[[ -n $(uname -a | grep -i 'budgie') ]] && dconf load / < bckpfiles/budgie-backup && echo "budgie-backup loaded" || echo "no budgie backup" 
+[[ -n $(screenfetch | grep -i 'budgie') ]] && dconf load / < bckpfiles/budgie-backup && echo "budgie-backup loaded" || echo "no budgie backup" 
     # "-n" means "if not empty string", "&&" means "on success"
     # "-z" means "if empty string",     "||" means "on fail"
 
 #remove errors.log when file is empty
-[ -z $(cat errors.log) ] && rm errors.log && echo "script finished without errors"
+[[ -z $(cat errors.log) ]] && rm errors.log && echo "script finished without errors"
 
 #handover to new user
 chown -R $usernam:$usernam ../first_one/

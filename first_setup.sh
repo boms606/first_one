89947c2e31905e0ad12dfaf3df7687a0cd960293
boms606 <sarialm@hotmail.com>
2020-03-23 04:10:33 +0100

    systemd check added

--- first_setup.sh
+++ first_setup.sh
@@ -19,11 +19,22 @@ cp scrip/* /home/$usernam/scripts/bash/
 
 #install some stuff
 apt update
-apt upgrade -y
-apt update
-apt install tmux htop snapd screenfetch -y
+apt upgrade -y && apt update
+apt install tmux htop screenfetch -y
 apt install g810-led -y
-apt update
+apt install skypeforlinux -y && apt update
+
+systemdizda() {
+    apt install snapd -y && apt update
+    snap install code --classic
+    snap install teams-for-linux
+    snap install vlc
+    snap install spotify
+    snap install obs-studio
+}
+
+pidof systemd && systemdizda || echo "No systemd active, thus snapd not necessary!"
+
 
 #set path to accept own scripts globally
 echo "" >> /etc/profile
@@ -35,9 +46,9 @@ echo "set -g default-terminal \"screen-256color\"" > /etc/tmux.conf
 echo -e "a 00006f\ng logo 6f0000\ng multimedia 006f00\ng indicators 006f00\nc" > /etc/g810-led/profile
 
 #load settings from backup if necessary
-[[ -n $(uname -a | grep -i 'budgie') ]] && dconf load / < bckpfiles/budgie-backup  
+[[ -n $(uname -a | grep -i 'budgie') ]] && dconf load / < bckpfiles/budgie-backup && echo "budgie-backup loaded" || echo "no budgie backup" 
     # "-n" means "if not empty string", "&&" means "on success"
-    # "-z" means "if empty string", "||" means "on fail"
+    # "-z" means "if empty string",     "||" means "on fail"
 
 #handover to new user
 chown -R $usernam:$usernam ../first_one/

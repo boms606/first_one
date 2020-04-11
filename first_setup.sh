f2004b4c9a2de5ea76cb04b470a79f9ca003c4f6
boms606 <sarialm@hotmail.com>
2020-04-11 16:02:36 +0200

    versionup - check for user, cleanafterwaqrds, \ng810 troubleshooting not finished!

--- first_setup.sh
+++ first_setup.sh
@@ -6,8 +6,23 @@
 #redirect errors to logfile named "errors"
 exec 2> errors.log
 
+systemdizda() {
+    apt install snapd -y && apt update
+    snap install code --classic
+    snap install teams-for-linux
+    snap install vlc
+    snap install spotify
+    snap install obs-studio
+}
+
+cleanafterwards() {
+    [[ -z $(cat errors.log) ]] && rm errors.log && echo "script finished without errors" || echo "errors occured"
+}
+
 #usernam=$(whoami)
 usernam=$1
+
+[ -z "$usernam" ]  && echo "Please specify a user" && cleanafterwards && exit 1
 
 #create directories 
 mkdir /home/$usernam/scripts
@@ -20,19 +35,12 @@ cp scrip/* /home/$usernam/scripts/bash/
 apt update
 apt upgrade -y && apt update
 apt install tmux htop screenfetch g++ make -y
-apt install g810-led -y
+apt install g810-led -y # || git install ..
 # https://github.com/MatMoul/g810-led/blob/master/INSTALL.md
-apt install skypeforlinux -y && apt update
+apt install skypeforlinux -y
+apt update
 
-systemdizda() {
-    apt install snapd -y && apt update
-    snap install code --classic
-    snap install teams-for-linux
-    snap install vlc
-    snap install spotify
-    snap install obs-studio
-}
-
+# check whether systemd is present
 pidof systemd && systemdizda || echo "No systemd active, thus snapd not necessary!"
 
 
@@ -50,11 +58,13 @@ echo -e "a 00006f\ng logo 6f0000\ng multimedia 006f00\ng indicators 006f00\nc" >
     # "-n" means "if not empty string", "&&" means "on success"
     # "-z" means "if empty string",     "||" means "on fail"
 
-#remove errors.log when file is empty
-[[ -z $(cat errors.log) ]] && rm errors.log && echo "script finished without errors" || echo "errors occured"
-
 #handover to new user
 chown -R $usernam:$usernam ../first_one/
 chown -R $usernam:$usernam /home/$usernam/scripts/
 
+#remove errors.log when file is empty
+cleanafterwards
+
 #to be continued later 
+
+exit 0

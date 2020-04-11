2b37a8b36fe2bd23f06e06a7980dd657a7fcbaa9
boms606 <sarialm@hotmail.com>
2020-04-11 20:13:50 +0200

    minor aesthetics

--- first_setup.sh
+++ first_setup.sh
@@ -4,7 +4,7 @@
 #       - add timestamps to errors.log logs
 #       - troubleshooting for apt install g810 for older debian versions (install via git)
 
-#redirect errors to logfile named "errors"
+# redirect errors to logfile named "errors"
 exec 2> errors.log
 
 systemdizda() {
@@ -20,22 +20,24 @@ systemdizda() {
 }
 
 cleanafterwards() {
+    # remove errors.log when file is empty 
     [[ -z $(cat errors.log) ]] && rm errors.log && echo "script finished without errors" || echo "errors occured"
 }
 
-#usernam=$(whoami)
+# usernam=$(whoami)
 usernam=$1
 
+# user should be specified - whoami does not work while the script should be executed as root
 [ -z "$usernam" ]  && echo "Please specify a user" && cleanafterwards && exit 1
 
-#create directories 
+# create directories 
 mkdir /home/$usernam/scripts
 mkdir /home/$usernam/scripts/bash
 
-#copy scripts
+# copy scripts
 cp scrip/* /home/$usernam/scripts/bash/
 
-#install some stuff
+# install some stuff
 apt update
 apt upgrade -y && apt update
 apt install tmux htop screenfetch g++ make -y
@@ -49,28 +51,28 @@ apt update
 pidof systemd && systemdizda || echo "No systemd active, thus snapd not necessary!"
 
 
-#set path to accept own scripts globally
+# set path to accept own scripts globally
 export PATH=$PATH:/home/$usernam/scripts/bash && pathtobeadded+=:/home/$usernam/scripts/bash
 
-#set personalisation variables
+# set personalisation variables
 echo "set -g default-terminal \"screen-256color\"" > /etc/tmux.conf
 echo -e "a 00006f\ng logo 6f0000\ng multimedia 006f00\ng indicators 006f00\nc" > /etc/g810-led/profile
 
-#load settings from backup if necessary
+# load settings from backup if necessary
 [[ -n $(screenfetch | grep -qi 'budgie') ]] && dconf load / < bckpfiles/budgie-backup && echo "budgie-backup loaded" || echo "no budgie backup" 
     # "-n" means "if not empty string", "&&" means "on success"
     # "-z" means "if empty string",     "||" means "on fail"
 
-#handover to new user
+# handover to new user
 chown -R $usernam:$usernam ../first_one/
 chown -R $usernam:$usernam /home/$usernam/scripts/
 
+# make changes to path permanent
 echo "" >> /etc/profile
 echo "export PATH=$PATH$pathtobeadded" >> /etc/profile
 
-#remove errors.log when file is empty
 cleanafterwards
 
-#to be continued later 
+# to be continued later 
 
 exit 0
